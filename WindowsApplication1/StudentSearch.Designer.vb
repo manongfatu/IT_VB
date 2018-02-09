360c74462e339e061867006ba0e51b39388eff73
Jurris <manongfatu@gmail.com>
2018-02-09 20:20:22 +0800

    Initial commit

---
+++ WindowsApplication1/StudentSearch.Designer.vb
@@ -0,0 +1,167 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class StudentSearch
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.sidepanel = New System.Windows.Forms.Panel()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.studentList = New System.Windows.Forms.TextBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.sidepanel.SuspendLayout()
+        Me.Panel4.SuspendLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'sidepanel
+        '
+        Me.sidepanel.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.sidepanel.Controls.Add(Me.PictureBox2)
+        Me.sidepanel.Controls.Add(Me.Panel3)
+        Me.sidepanel.Controls.Add(Me.Panel2)
+        Me.sidepanel.Location = New System.Drawing.Point(930, 54)
+        Me.sidepanel.Name = "sidepanel"
+        Me.sidepanel.Size = New System.Drawing.Size(234, 445)
+        Me.sidepanel.TabIndex = 0
+        '
+        'Panel3
+        '
+        Me.Panel3.Location = New System.Drawing.Point(202, 0)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(763, 57)
+        Me.Panel3.TabIndex = 1
+        '
+        'Panel2
+        '
+        Me.Panel2.Location = New System.Drawing.Point(202, 0)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(766, 36)
+        Me.Panel2.TabIndex = 1
+        '
+        'Panel4
+        '
+        Me.Panel4.BackColor = System.Drawing.Color.Maroon
+        Me.Panel4.Controls.Add(Me.Label1)
+        Me.Panel4.Controls.Add(Me.PictureBox1)
+        Me.Panel4.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel4.Location = New System.Drawing.Point(0, 0)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(968, 54)
+        Me.Panel4.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Product Sans", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.HighlightText
+        Me.Label1.Location = New System.Drawing.Point(61, 16)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(324, 23)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "University Of Southeastern Philippines"
+        '
+        'studentList
+        '
+        Me.studentList.Location = New System.Drawing.Point(24, 88)
+        Me.studentList.Name = "studentList"
+        Me.studentList.Size = New System.Drawing.Size(277, 20)
+        Me.studentList.TabIndex = 2
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.WindowsApplication1.My.Resources.Resources.icon
+        Me.PictureBox2.Location = New System.Drawing.Point(3, 3)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(33, 27)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox2.TabIndex = 2
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.WindowsApplication1.My.Resources.Resources.useplogo
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 9)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(43, 39)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'ListView1
+        '
+        Me.ListView1.Location = New System.Drawing.Point(24, 114)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(621, 373)
+        Me.ListView1.TabIndex = 4
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Product Sans", 9.749999!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(24, 70)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(92, 15)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Student Name:"
+        '
+        'Form1
+        '
+        Me.AllowDrop = True
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(968, 499)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.studentList)
+        Me.Controls.Add(Me.sidepanel)
+        Me.Controls.Add(Me.Panel4)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.sidepanel.ResumeLayout(False)
+        Me.Panel4.ResumeLayout(False)
+        Me.Panel4.PerformLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents sidepanel As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents studentList As TextBox
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents Label2 As Label
+End Class
---
+++ WindowsApplication1/StudentSearch.vb
@@ -0,0 +1,88 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class StudentSearch
+
+    Private getQuery As String
+    Private getcommand As MySqlCommand
+    Private getReader As MySqlDataReader
+
+    Public sideBarStatus As Integer = 0
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+        If sideBarStatus = 0 Then
+
+
+            sidepanel.Location = New Point(930, 54)
+            Do Until sidepanel.Location.X = 734
+                sidepanel.Location = New Point(sidepanel.Location.X - 1, 54)
+                Refresh()
+                sideBarStatus = 1
+
+            Loop
+
+        ElseIf sideBarStatus = 1 Then
+
+            sidepanel.Location = New Point(734, 54)
+            Do Until sidepanel.Location.X = 930
+                sidepanel.Location = New Point(sidepanel.Location.X + 1, 54)
+                Refresh()
+                sideBarStatus = 0
+            Loop
+
+
+
+
+
+
+
+        End If
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles studentList.TextChanged
+        Try
+            getQuery = "SELECT id_number, last_name, first_name, middle_name, course, college, scholarship FROM basic_info where last_name like '%" & studentList.Text & "%' Or first_name Like '%" & studentList.Text & "%' Or id_number Like '%" & studentList.Text & "%' Or course Like '%" & studentList.Text & "%' Or college Like '%" & studentList.Text & "%'
+            Or middle_name Like '%" & studentList.Text & "%' or scholarship Like '%" & studentList.Text & "%'"
+
+
+            getcommand = New MySqlCommand(getQuery, conn)
+            getReader = getcommand.ExecuteReader
+            lvList.Items.Clear()
+
+            Do While getReader.Read()
+
+                id_n = (getReader("id_number").ToString)
+                first = (getReader("first_name").ToString)
+                middle = (getReader("middle_name").ToString)
+                last = (getReader("last_name").ToString)
+                coll = (getReader("college").ToString)
+                cour = (getReader("course").ToString)
+                schol = (getReader("scholarship").ToString)
+
+                With lvList.Items.Add(id_n)
+
+                    .SubItems.Add(first + " " + middle + " " + last)
+                    .SubItems.Add(coll)
+                    .SubItems.Add(cour)
+                    .SubItems.Add(schol)
+                End With
+
+            Loop
+            getReader.Close()
+            getcommand.Dispose()
+
+
+        Catch ex As Exception
+
+            MsgBox("No record!")
+        End Try
+
+
+    End Sub
+    End Sub
+End Class
