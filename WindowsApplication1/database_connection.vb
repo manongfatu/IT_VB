c20229448a2e305153f096fdbaf6646f5132e957
Jurris <manongfatu@gmail.com>
2018-02-09 21:46:14 +0800

    yo
    wasap

---
+++ WindowsApplication1/database_connection.vb
@@ -0,0 +1,25 @@
+﻿Imports MySql.Data.MySqlClient
+Module database_connection
+
+    Public MySQLConnection As New MySqlConnection
+
+
+    ' Connection to database goes here
+    Public Sub getMySQLConnection()
+
+        MySQLConnection.ConnectionString = "SERVER = localhost; USER = root; DATABASE = ease; PWD = "
+
+        Try
+
+            MySQLConnection.Open()
+        Catch showError As Exception
+
+            MsgBox(showError.ToString)
+
+        End Try
+
+    End Sub
+
+
+End Module
+
